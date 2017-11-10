e09d61c066dc9bb05ad3bcf82001ca4cc76277aa
2weirdy <simon2cheese@yahoo.com>
2017-11-10 16:13:20 +0100

    Implemented game winning and movement. gameOver will be true if a player has won the game.

--- Game/core/src/com/mygdx/game/BattleField/BattleFieldLogic.java
+++ Game/core/src/com/mygdx/game/BattleField/BattleFieldLogic.java
@@ -1,6 +1,8 @@
 package com.mygdx.game.BattleField;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by 2weirdy on 2017-11-10.
@@ -11,6 +13,7 @@ public class BattleFieldLogic {
     public final int height;
     public final MinionNode[][] field;
     public boolean isLeftPlayerTurn = true;
+    public boolean gameOver = false;
 
     public final ArrayList<MinionNode> leftPlayerMinions = new ArrayList<MinionNode>();
     public final ArrayList<MinionNode> rightPlayerMinions = new ArrayList<MinionNode>();
@@ -24,6 +27,8 @@ public class BattleFieldLogic {
     public boolean addMinion(MinionNode m, int x, int y){
         if(field[x][y]==null) {
             field[x][y] = m;
+            m.minion.xPos = x;
+            m.minion.yPos = y;
             if(m.minion.isLeftPlayer){
                 leftPlayerMinions.add(m);
             }else{
@@ -39,15 +44,62 @@ public class BattleFieldLogic {
         return null;
     }
 
-    public void doGameStep(){
+    /**
+     *
+     * @return returns true if a player has won. Which player has won can be obtained from the field isLeftPlayerTurn.
+     */
+    public boolean doGameStep(){
+        if(gameOver) return true;
         if(isLeftPlayerTurn){
-            for(int x = width-1; x>=0; --x){
-                for(int y = 0; y<height; --y){
-                    if(field[x][y]!= null){
-
+            for(MinionNode n:leftPlayerMinions){
+                Minion m = n.minion;
+                if(field[m.xPos+1][m.yPos]==null){
+                    ++m.xPos;
+                    if(m.xPos == width-1){
+                        gameOver = true;
+                        return true;
                     }
+                    field[m.xPos+1][m.yPos] = n;
+                    field[m.xPos][m.yPos] = null;
                 }
             }
+            Collections.sort(leftPlayerMinions, new Comparator<MinionNode>() {
+                @Override
+                public int compare(MinionNode t0, MinionNode t1) {
+                    if(t0.minion.xPos<t1.minion.xPos){
+                        return -1;
+                    }
+                    if(t0.minion.xPos>t1.minion.xPos){
+                        return 1;
+                    }
+                    return 0;
+                }
+            });
+        }else{
+            for(MinionNode n:rightPlayerMinions){
+                Minion m = n.minion;
+                if(field[m.xPos-1][m.yPos]==null){
+                    --m.xPos;
+                    if(m.xPos == 0){
+                        gameOver = true;
+                        return true;
+                    }
+                    field[m.xPos+1][m.yPos] = n;
+                    field[m.xPos][m.yPos] = null;
+                }
+            }
+            Collections.sort(rightPlayerMinions, new Comparator<MinionNode>() {
+                @Override
+                public int compare(MinionNode t0, MinionNode t1) {
+                    if(t0.minion.xPos<t1.minion.xPos){
+                        return 1;
+                    }
+                    if(t0.minion.xPos>t1.minion.xPos){
+                        return -1;
+                    }
+                    return 0;
+                }
+            });
         }
     }
 
